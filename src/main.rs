ff9b6be95b038ffe8684d692d4b0d22e60e27f4b
Théo Posty <theo@idfr.net>
2022-04-07 14:32:05 +0200

    add file edit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::fs;
+use std::{
+    fs::{self, OpenOptions},
+    io::Write,
+};
+
+const LIGHT_THEME: &str = "Arc";
+const DARK_THEME: &str = "Arc-Dark";
 
 fn main() -> Result<(), systray::Error> {
     let mut app;
@@ -12,11 +18,15 @@ fn main() -> Result<(), systray::Error> {
     );
 
     let file_content = fs::read_to_string(file_path).expect("Failed to read xsettings file");
-    let theme = file_content.split("\"").collect::<Vec<&str>>()[1];
 
-    let icon_file = if theme.to_lowercase().contains("dark") {
+    let file_theme = file_content.split("\"").collect::<Vec<&str>>()[1];
+    let mut theme: String;
+
+    let icon_file = if file_theme.to_lowercase().contains("dark") {
+        theme = String::from("dark");
         "./assets/moon.svg"
     } else {
+        theme = String::from("light");
         "./assets/sun.svg"
     };
 
@@ -26,8 +36,57 @@ fn main() -> Result<(), systray::Error> {
     }
     app.set_icon_from_file(icon_file)?;
 
-    app.add_menu_item("Change theme", |_| {
+    app.add_menu_item("Change theme", move |app| {
         println!("Changing theme");
+
+        match theme.as_str() {
+            "dark" => {
+                app.set_icon_from_file("./assets/sun.svg")?;
+
+                let new_str = format!("Net/ThemeName \"{}\"", LIGHT_THEME);
+                theme = "light".to_string();
+
+                let mut file = OpenOptions::new()
+                    .write(true)
+                    .truncate(true)
+                    .open(format!(
+                        "{}/.xsettingsd",
+                        dirs::home_dir()
+                            .unwrap()
+                            .into_os_string()
+                            .into_string()
+                            .unwrap()
+                    ))
+                    .unwrap();
+                file.write_all(new_str.as_bytes()).unwrap();
+                file.flush().unwrap();
+            }
+            "light" => {
+                app.set_icon_from_file("./assets/moon.svg")?;
+
+                let new_str = format!("Net/ThemeName \"{}\"", DARK_THEME);
+                theme = "dark".to_string();
+
+                let mut file = OpenOptions::new()
+                    .write(true)
+                    .truncate(true)
+                    .open(format!(
+                        "{}/.xsettingsd",
+                        dirs::home_dir()
+                            .unwrap()
+                            .into_os_string()
+                            .into_string()
+                            .unwrap()
+                    ))
+                    .unwrap();
+                file.write_all(new_str.as_bytes()).unwrap();
+                file.flush().unwrap();
+            }
+            _ => {
+                println!("ok")
+            }
+        }
+
         Ok::<_, systray::Error>(())
     })?;
 
